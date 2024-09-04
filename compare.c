a6f2d378a28af3d89919a979c588af2d0ea6b3a4
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-09-04 00:36:41 +0000

    Wed, Sep 4, 2024, 6:06 AM +05:30

--- compare.c
+++ compare.c
@@ -7,6 +7,11 @@ int main(void){
 
     if (x<y)
     {
-        printf("x is less than y");
+        printf("x is less than y\n");
+    }
+
+    else
+    {
+        printf("x is greater than y\n");
     }
 }
