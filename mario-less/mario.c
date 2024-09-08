0febb94f2d397af9fb79477f5bbfddb94a6e0f3c
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-09-08 12:22:59 +0000

    Sun, Sep 8, 2024, 5:52 PM +05:30

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -3,6 +3,16 @@
 
 int main(void)
 {
-    int height = get_int("Height: ");
-    
+    int height;
+    do
+    {
+        height = get_int("Enter Height:");
+    }
+    while (height<1 || height>0);
+
+    for(row = 0; row < height; row++)
+    {
+        printf("/n")
+    }
+
 }
