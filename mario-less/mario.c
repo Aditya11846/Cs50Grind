ad9561116953d691863856a7ca39c18c279e07b8
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-09-09 23:12:18 +0000

    Tue, Sep 10, 2024, 4:42 AM +05:30

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -19,12 +19,14 @@ int main(void)
 
     for (row = 0 ; row < height; row++)
     {
+        for(space = 0; space < height - row - 1; space++)
+        {
+            printf(" ");
+        }
+
         for(column = 0; column <= row; column++)
         {
-
-
             printf("#");
-
         }
 
         printf("\n");
