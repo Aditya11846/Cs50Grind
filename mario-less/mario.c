82af7fc4530cdab6df60c750635a8cf90303df31
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-09-09 18:04:26 +0000

    Mon, Sep 9, 2024, 11:34 PM +05:30

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -4,25 +4,30 @@
 int main(void)
 {
     int height;
+
+    // Prompt for height between 1 and 8 inclusive
     do
     {
-        height = get_int("Enter Height:");
+        height = get_int("Enter Height: ");
     }
-    while (height<1|| height>8 );
+    while (height < 1 || height > 8);
 
-    for(int row = 0; row < height; row++)
+    // Loop through rows
+    for (int row = 0; row < height; row++)
     {
-        for ( int hash = 0; int column = 0; column<height; column++; hash++)
+        // Print spaces for left side of the pyramid
+        for (int space = 0; space < height - row - 1; space++)
+        {
+            printf(" ");
+        }
+
+        // Print hashes for the pyramid
+        for (int hash = 0; hash <= row; hash++)
         {
             printf("#");
-
-            if (column = hash)
-            {
-                printf("\n");
-            }
-
         }
+
+        // Move to the next line after completing a row
         printf("\n");
     }
-
 }
