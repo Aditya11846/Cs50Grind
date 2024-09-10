7d5d545cf741d4397f22fae3a8089edd1614cb5d
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-09-10 19:00:11 +0000

    Wed, Sep 11, 2024, 12:30 AM +05:30

--- cash/cash.c
+++ cash/cash.c
@@ -4,50 +4,6 @@
 int main(void)
 {
     int change;
-
-    change = get_int("What is the Change: ");
-
-    int q1 = change/25;
-    int q2 = change/10;
-    int q3 = change/5;
-    int q4 = change/1;
-    int small = 0;
-
-    if(change%q1 == 0 || change%q2 == 0 || change%q3 == 0  )
-    {
-
-    if(q1<q2)
-    {
-        small = q1;
-
-    }
-    else
-    {
-        small = q2;
-    }
-    if(q1<q3)
-    {
-        small = q1;
-    }
-    else
-    {
-        small = q3;
-    }
-    if(q1<q4)
-    {
-        small = q1;
-    }
-    else
-    {
-        small = q4;
-    }
-
-
-    printf("%d\n",small);
-    }
-    else
-    {
-        printf("Sorry change not available");
-    }
-
+    change = get_int("What is the change ? ");
+    
 }
