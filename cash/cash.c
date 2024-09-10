51a3673c1dea6fa2ce38781218d5eee293dceca0
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-09-10 18:24:54 +0000

    Tue, Sep 10, 2024, 11:54 PM +05:30

--- cash/cash.c
+++ cash/cash.c
@@ -10,7 +10,7 @@ int main(void)
     price = get_int("What is the Price: ");
     paid = get_int("How much are you paying: ");
 
-    change = paid- price;
+    change = price-paid;
 
     int q1 = change/25;
     int q2 = change/10;
@@ -19,5 +19,9 @@ int main(void)
 
     printf("%d %d %d %d\n", q1,q2,q3,q4);
 
-
+    if(q1<q2)
+    {
+        q1 = q2;
+        printf("%d",q2);
+    }
 }
