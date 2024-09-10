b7dcd6d972e8844d84f27354db58c6a40e9c512f
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-09-10 19:26:41 +0000

    Wed, Sep 11, 2024, 12:56 AM +05:30

--- cash/cash.c
+++ cash/cash.c
@@ -7,5 +7,17 @@ int main(void)
     change = get_int("What is the change ? ");
 
     int quarters = change/25;
-    change = change%25
+    change = change%25;
+
+    int dime = change/10;
+    change = change%10;
+
+    int nickel = change/5;
+    change = change%5;
+
+    int penny = change/5;
+    change= penny%5;
+
+    int final_change = quarters + dime + nickel + penny;
+    printf("%d\n", final_change);
 }
