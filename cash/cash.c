9a5ede57750d0dfc85cbbe13cdc4d95a6f06780b
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-09-10 18:14:17 +0000

    Tue, Sep 10, 2024, 11:44 PM +05:30

--- cash/cash.c
+++ cash/cash.c
@@ -10,5 +10,13 @@ int main(void)
     price = get_int("What is the Price: ");
     change = paid- price;
 
-    
+    int r1 = change%25;
+    int r2 = change%10;
+    int r3 = change%5;
+    int r4 = change%1;
+
+    if(r1<r2);
+    {
+        
+    }
 }
