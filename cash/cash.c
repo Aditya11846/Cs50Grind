4461e685aba9272e393af5e08afed53239897d46
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-09-10 18:52:01 +0000

    Wed, Sep 11, 2024, 12:22 AM +05:30

--- cash/cash.c
+++ cash/cash.c
@@ -13,7 +13,7 @@ int main(void)
     int q4 = change/1;
     int small = 0;
 
-    if(change%q1 == 0 || change%q2 == 0 || change%q3 == 0 || change%q4 == 0)
+    if(change%q1 == 0 || change%q2 == 0 || change%q3 == 0  )
     {
 
     if(q1<q2)
@@ -45,8 +45,9 @@ int main(void)
 
     printf("%d\n",small);
     }
-
-    printf("Sorry change not available");
-
+    else
+    {
+        printf("Sorry change not available");
+    }
 
 }
