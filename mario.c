5f9cc8ea2c07cab88e5b58abfba42ecd4ab5e705
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-09-10 11:18:53 +0000

    Tue, Sep 10, 2024, 4:48 PM +05:30

--- mario.c
+++ mario.c
@@ -3,5 +3,22 @@
 
 int main(void)
 {
-    
+    int height;
+    int row;
+    int column;
+
+    do
+    {
+        height = get_int("Enter Height here: ");
+    }
+    while(height<1);
+
+    for(row = 0; row<height; row++)
+    {
+        for(column = 0 ; column<=row; column++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
