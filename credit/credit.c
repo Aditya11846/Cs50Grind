25e1d4b29aeb490f4b083ad00466167f994b22f2
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-09-11 11:11:10 +0000

    Wed, Sep 11, 2024, 4:41 PM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -6,10 +6,12 @@ int main(void)
     long number = get_long("Enter the number\n");
 
     long temp_number = number;
-    int length_count;
+    int length_count = 0;
 
-    while(temp_number>0)
+    while(temp_number != 0)
     {
-        temp_number/10
+        temp_number = number/10;
+        length_count++;
     }
+    printf("%d",length_count);
 }
