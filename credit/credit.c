d6148ac759733835d16192d9a9643fdbc41a8ea5
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-09-11 11:26:07 +0000

    Wed, Sep 11, 2024, 4:56 PM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void)
+
+long get_card_length(long)
 {
-    long number = get_long("Enter the number\n");
+
 
     long temp_number = number;
     long length_count = 0;
@@ -14,7 +15,19 @@ int main(void)
         length_count++;
     }
 
+    return length_count;
+
     printf("%ld",length_count);
 
 
+
 }
+
+
+int main(void)
+{
+    long number = get_long("Enter the number\n");
+
+    get_card_length(number);
+}
+
