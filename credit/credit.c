7e8a132daa26d1198cb2a2eae83cdabc5bac11d2
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-09-11 09:14:20 +0000

    Wed, Sep 11, 2024, 2:44 PM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -27,7 +27,7 @@ int main(void)
     // Call the function to get the length of the card number
     int length = get_card_length(card);
     }
-    while(length)
+    while(length!== 13)
 
     // Print the result
     printf("Number of digits: %d\n", length);
