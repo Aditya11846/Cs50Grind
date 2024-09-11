d13ae130440dff29a77950a2531e65614ad57c8b
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-09-11 11:05:26 +0000

    Wed, Sep 11, 2024, 4:35 PM +05:30

---
+++ credit/credit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    long number = get_long("Enter the number\n");
+
+    int length;
+    int temp_number;
+
+    while(length>0)
+    {
+        
+    }
+}
