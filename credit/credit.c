a464a4f284e30d374f168f7735f0c242478194a5
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-09-11 22:25:17 +0000

    Thu, Sep 12, 2024, 3:55 AM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -6,7 +6,7 @@ int get_length(long number)
 {
     int length = 0;
     long temp = number;
-    while(temp != 0)
+    while (temp != 0)
     {
         temp = temp / 10;
         length++;
@@ -22,7 +22,7 @@ bool check_sum(long number)
 
     while (number > 0)
     {
-        if (var == true)
+        if (var)
         {
             total += number % 10;
             number /= 10;
@@ -45,33 +45,48 @@ bool check_sum(long number)
         }
     }
 
-    // Check if total sum ends in 0
     return (total % 10 == 0);
 }
 
 int main(void)
 {
-    int length;
-    long number;
-
-    do
-    {
-        number = get_long("What is the credit card number: ");
-
-        // Use the function to calculate the length of the card number
-        length = get_length(number);
-
-    } while (length != 13 && length != 15 && length != 16);
-
-    printf("The number of digits are: %d\n", length);
+    long number = get_long("What is the credit card number: ");
+    int length = get_length(number);
 
     // Check if the card number is valid using the checksum function
-    if (check_sum(number))
+    if (!check_sum(number))
     {
-        printf("The card number is valid!\n");
+        printf("INVALID\n");
+        return 0;
+    }
+
+    // Determine card type
+    int first_digit = number;
+    while (first_digit >= 10)
+    {
+        first_digit /= 10;
+    }
+
+    int first_two_digits = number;
+    while (first_two_digits >= 100)
+    {
+        first_two_digits /= 10;
+    }
+
+    if (length == 15 && first_two_digits == 34 || first_two_digits == 37)
+    {
+        printf("AMEX\n");
+    }
+    else if (length == 16 && first_two_digits >= 51 && first_two_digits <= 55)
+    {
+        printf("MASTERCARD\n");
+    }
+    else if ((length == 13 || length == 16) && first_digit == 4)
+    {
+        printf("VISA\n");
     }
     else
     {
-        printf("The card number is invalid!\n");
+        printf("INVALID\n");
     }
 }
