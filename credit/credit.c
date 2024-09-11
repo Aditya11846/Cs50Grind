e5fd2c6a4a1a910eefe90d67f00de5f6fdb3aa89
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-09-11 08:53:59 +0000

    Wed, Sep 11, 2024, 2:23 PM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -6,23 +6,9 @@ int main(void)
     int card;
     card = get_long("Enter your card number: \n");
 
-    long length_counter;
-    int r;
-    int q;
+    long card_length;
+    int q
+    int r
 
-
-    for(length_counter = 0;length_counter> card ;length_counter++)
-    {
-        for(r=0; r > 0 ; r++)
-    {
-        q = card/10 ;
-        r = card%10;
-
-    }
-
-    }
-     printf("%ld" , length_counter);
-
-
-
+    
 }
