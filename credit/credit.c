4636710430891d14913934d06956373f3efad3c4
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-09-11 21:16:19 +0000

    Thu, Sep 12, 2024, 2:46 AM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -8,19 +8,18 @@ int main(void)
 
     do
     {
-    number = get_long("What is the credit card number: ");
+        number = get_long("What is the credit card number: ");
 
-    //Checking the length of the given card number//
-
-    long temp = number;
-    length = 0;
-    while(temp!=0)
-    {
-        temp = temp/10;
-        length++;
+        // Checking the length of the given card number
+        long temp = number;
+        length = 0;
+        while(temp != 0)
+        {
+            temp = temp / 10;
+            length++;
+        }
     }
+
+    while(length != 13 && length != 15 && length != 16);
     printf("The number of digits are: %d\n", length);
-    }
-
-    while(length!=13&&length!=15&&length!=16);
 }
