c4bc44208b1efbee3efa5d1538dcc22321c64b6f
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-09-12 20:56:41 +0000

    Fri, Sep 13, 2024, 2:26 AM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -3,19 +3,23 @@
 
 int main(void)
 {
-    long number = get_long("Please enter your card number");
+    long number = get_long("Please enter your card number ");
 
-
-}
-
-bool length_calculator ( long number)
-{
+    //Length calculator//
     int temp = number;
     int length = 0;
 
-    while()
+    while(temp!= 0)
     {
         temp /= 10;
         length++;
     }
+    printf("This is a %d digit number \n" , length);
+
+
+    //Luhn's algorithm Checksum//
+
+    
+
 }
+
