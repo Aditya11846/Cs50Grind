4ec456f855fd0edb64cbdd8273d0966014716b12
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-09-15 10:38:48 +0000

    Sun, Sep 15, 2024, 4:08 PM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -1,60 +1,66 @@
-#include<stdio.h>
-#include<cs50.h>
+#include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
     int length;
     long input;
 
-    //Calculate and loop length to be 13 , 15 or 16 only//
+    // Calculate and loop until the length is 13, 15, or 16 only
     do
     {
+        input = get_long("Please enter your number here:\n ");
+        long temp = input;
+        length = 0;
 
-    input = get_long("Please enter your number here:\n ");
-    long temp = input;
-    length = 0;
-
-    while(temp!= 0)
-    {
-        temp = temp/10;
-        length++;
+        while (temp != 0)
+        {
+            temp = temp / 10;
+            length++;
+        }
     }
-    }
-    while(length != 13 && length != 15 && length != 16 );
+    while (length != 13 && length != 15 && length != 16);
 
-    //Luhn's Checksum//
+    // Luhn's Checksum Calculation
     long luhn_input = input;
+    int sum = 0;
     int digit_counter = 0;
     int digit;
 
-    while(luhn_input>0)
+    while (luhn_input > 0)
     {
+        // Extract the last digit
+        digit = luhn_input % 10;
 
-    luhn_input /= 10;
-    digit = luhn_input%10;
-    digit_counter++;
-    int sum = 0;
+        // Remove the last digit
+        luhn_input /= 10;
 
-    if(digit_counter%2 != 0)
-    {
-        digit *= 2;
-        if(digit>10)
+        digit_counter++;
+
+        // Double every second digit from the right (every odd counter from the end)
+        if (digit_counter % 2 == 0)
         {
-            digit = (digit/10) + (digit%10);
+            digit *= 2;
+            // If doubling the digit makes it > 9, sum the digits of the product
+            if (digit > 9)
+            {
+                digit = (digit / 10) + (digit % 10);
+            }
         }
-        sum += digit;
 
-    else
-    {
+        // Add the digit (whether doubled or not) to the total sum
         sum += digit;
     }
-    }
 
+    // Check if the total sum is a multiple of 10
+    if (sum % 10 == 0)
+    {
+        printf("The number is valid according to Luhn's algorithm.\n");
+    }
     else
     {
-        sum+= digit;
+        printf("The number is not valid according to Luhn's algorithm.\n");
     }
 
-    }
-
+    return 0;
 }
