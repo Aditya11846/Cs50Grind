daa08408094e13b42a73b8df349c0228e40e8b6c
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-09-25 18:05:11 +0000

    Wed, Sep 25, 2024, 11:35 PM +05:30

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
-# include <stdio.h>
-# include <cs50.h>
+#include <stdio.h>
+#include <cs50.h>
+
 int main(void)
 {
-    string answer = get_string("Hello what is your name ? ");
-    printf("Hello, %s%s!\n", answer, answer );
+    printf("Hello World!\n");
 }
