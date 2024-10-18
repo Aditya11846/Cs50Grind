e2a642c8847daf22ac25b1782f8da48511048bdc
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-10-18 07:15:18 +0000

    Fri, Oct 18, 2024, 12:45 PM +05:30

--- scores.c
+++ scores.c
@@ -2,9 +2,22 @@
 #include <cs50.h>
 
 const int N = 3;
+float average( int array[]);
 
 
 int main(void)
+{
+    int scores[N];
+    for(int i = 0; i<N; i++)
+    {
+        scores[i]= get_int("Score: ");
+    }
+
+    printf("The Average is : %f\n", average(scores));
+
+}
+
+float average(int array[])
 {
 
 }
