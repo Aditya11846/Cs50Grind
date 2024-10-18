a022278aeec521081943187508ab45480198fdb9
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-10-18 06:18:15 +0000

    Fri, Oct 18, 2024, 11:48 AM +05:30

--- scores.c
+++ scores.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    int
+    int scores[3];
+
+    for (int i = 0; i<3; i++)
+    {
+        scores[i] = get_int("Score: ");
+    }
+
+    printf("Average Score : %f\n ", (scores[0]+scores[1]+scores[2])/3.0);
 }
