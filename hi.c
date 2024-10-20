62da07ecc840cb3b62be89604e7eada49143cae6
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-10-20 06:57:35 +0000

    Sun, Oct 20, 2024, 12:27 PM +05:30

--- hi.c
+++ hi.c
@@ -7,6 +7,6 @@ int main(void)
     words[0]="HI!";
     words[1]="BYE!";
 
-    printf("%c%c%C\n", words[0]);
-    printf("%s",words[1]);
+    printf("%c%c%c\n", words[0][0],words[0][1],words[0][2]);
+    printf("%c%c%c%C\n",words[1][0],words[1][1],words[1][2],words[1][3]);
 }
