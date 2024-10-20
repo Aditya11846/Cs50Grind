a44cc253627067dc088e418ccc3a65f6c5fd6734
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-10-20 06:03:56 +0000

    Sun, Oct 20, 2024, 11:33 AM +05:30

--- scores.c
+++ scores.c
@@ -5,8 +5,8 @@
 int main(void)
 {
     int score1 = 72;
-    int score2 = 34;
+    int score2 = 32;
     int score3 = 86;
 
-    printf("Average = %f\n", (score1+score2+score3)/3);
+    printf("Average = %f\n", (score1+score2+score3)/ (float) 3);
 }
