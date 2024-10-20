087625113fbdf5c7dafca4d14a913438ac0e3942
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-10-20 07:14:57 +0000

    Sun, Oct 20, 2024, 12:44 PM +05:30

--- uppercase.c
+++ uppercase.c
@@ -7,11 +7,16 @@ int main(void)
     string s = get_string("Before: ");
     printf("After: ");
 
-    for(int i = 0; i<strlen(s),i++)
+    for(int i = 0; i<strlen(s);i++)
     {
         if(s[i] >= 'a'&& s[i] <= 'z')
         {
             printf("%c",s[i] - 32);
+        }
+
+        else
+        {
+            printf("%c",s[i]);
         }
     }
 }
