a8bae6f9cc060dae048d95b2c0b545e2889b3da1
Aditya Singh <102846502+Aditya11846@users.noreply.github.com>
2024-10-20 07:18:43 +0000

    Sun, Oct 20, 2024, 12:48 PM +05:30

--- uppercase.c
+++ uppercase.c
@@ -10,15 +10,9 @@ int main(void)
 
     for(int i = 0; i<strlen(s);i++)
     {
-        if(s[i] >= 'a'&& s[i] <= 'z')
-        {
-            printf("%c",s[i] - 32);
-        }
+         printf("%c",toupper(s[i]));
 
-        else
-        {
-            printf("%c",s[i]);
-        }
     }
+
     printf("\n");
 }
